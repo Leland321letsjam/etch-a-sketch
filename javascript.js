e131fdb30f2a3c81e812dbd8bf2d81d3b1481dcf
Leland321 <lelandwrites@outlook.com>
2024-09-07 18:47:48 -0400

    Generate new grid with attached button

--- javascript.js
+++ javascript.js
@@ -1,9 +1,51 @@
 function initializeGrid() {
     for (let i = 0; i < 256; i++) {
         const gridSquare = document.createElement('div');
+        gridSquare.classList.add('gridSquares');
         const gridContainer = document.querySelector('.grid-container');
         gridContainer.appendChild(gridSquare);
     }
 };
 
-initializeGrid();
+//initializeGrid();
+
+function generateNewGrid() {
+    let gridSize = prompt('Please enter a number 1-100');
+
+    while(gridSize > 100 || gridSize < 1) {
+        if (gridSize === null) {
+            break;
+        } else {
+            alert('Number must be between 1 and 100');
+            gridSize = prompt('Please enter a number 1-100');
+        }
+        
+    }
+
+    gridSize = Math.round(gridSize);
+    gridSize = gridSize * gridSize;
+
+    const removeOldGrid = document.querySelectorAll('.gridSquares');
+    if (removeOldGrid){
+        removeOldGrid.forEach(div => {
+            div.remove();
+        });
+    }
+
+    for (let i = 0; i < gridSize; i++) {
+        const gridSquare = document.createElement('div');
+        gridSquare.classList.add('gridSquares');
+        const gridContainer = document.querySelector('.grid-container');
+        gridContainer.appendChild(gridSquare);
+    }
+
+}
+
+const newGridButton = document.querySelector('.generate-new-grid');
+newGridButton.addEventListener('click', generateNewGrid);
+
+
+
+
+//testing:
+
